e1824ec817dcc0a49b2edfebb98731d695a936da
Vamshihv17 <158277311+Vamshihv17@users.noreply.github.com>
2025-07-12 20:57:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        primary: '#2563EB',
+        secondary: '#14B8A6',
+      },
+    },
+  },
+}
